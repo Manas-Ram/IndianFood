43fefdfd2e3d712f4f3be2a6f23f2b488b75cb27
Manas-Ram <manas.ramesh24@gmail.com>
2020-07-31 16:06:47 +0000

    herokutwo

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from model import get_nut_value
+from datetime import datetime
 
 
 # -- Initialization section --
@@ -12,17 +13,19 @@ app = Flask(__name__)
 
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods= ['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", time=datetime.now())
 
-
+@app.route('/foods', methods= ['GET', 'POST'])
+def foods():
+    return render_template("foods.html", time=datetime.now())
 @app.route('/result', methods= ['GET', 'POST'])
 def result():
     print(request.form)
     ind_food = (request.form["food"])
     quant_v=(request.form["quantity"])
     nut_v=get_nut_value(ind_food, quant_v)
-    return render_template("result.html", ind_food=ind_food, quant_v=quant_v, nut_v=nut_v)
+    return render_template("result.html", ind_food=ind_food, quant_v=quant_v, nut_v=nut_v, time=datetime.now())
 
 
--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 biryani=[140, 18.6, 4.5, 5.5, 298]
 samosa=[308, 32.2, 17.9, 4.7, 819]
-dosa=["489", "82.3", "11.2", "14.6", "884"]
-uttapam=["92", "14.2", "2.8", "17.9", "7"]
-idli=["33", "7.2", ".1", "1", "1.1"]
-basmati=["148", "32.4", "0", "3.5", "0"]
+dosa=[489, 82.3, 11.2, 14.6, 884]
+uttapam=[92, 14.2, 2.8, 17.9, 7]
+idli=[33, 7.2, .1, 1, 1.1]
+basmati=[148, 32.4, 0, 3.5, 0]
 def get_nut_value(food, quantity): 
     print(food)
     if food.lower() == "chicken biryani": 
@@ -11,15 +11,13 @@ def get_nut_value(food, quantity):
         #print("Calories " + int(quantity)*biryani(0)  + "Carbs " + int(quantity)*biryani(1) + "Fats " + int(quantity)*biryani(2) + "Protein " + int(quantity)*biryani(3) + "Sodium " + int(quantity)*biryani(4))
     elif food.lower() == "samosa":
         return "Calories " + str(int(quantity)*samosa[0])  + " Carbs " + str(int(quantity)*samosa[1]) + " Fats " + str(int(quantity)*samosa[2]) + " Protein " + str(int(quantity)*samosa[3]) + " Sodium " + str(int(quantity)*samosa[4])
-    # elif food.lower=="dosa":
-    #     return "Calories " + int(quantity)*dosa(0)  + "Carbs " + int(quantity)*dosa(1) + "Fats " + int(quantity)*dosa(2) + "Protein " + int(quantity)*dosa(3) + "Sodium " + int(quantity)*dosa(4)
-    # elif food.lower=="uttapam":
-    #     return "Calories " + int(quantity)*uttapam(0)  + "Carbs " + int(quantity)*uttapam(1) + "Fats " + int(quantity)*uttapam(2) + "Protein " + int(quantity)*uttapam(3) + "Sodium " + int(quantity)*uttapam(4)
-    # elif food.lower=="idli":
-    #     return "Calories " + int(quantity)*idli(0)  + "Carbs " + int(quantity)*idli(1) + "Fats " + int(quantity)*idli(2) + "Protein " + int(quantity)*idli(3) + "Sodium " + int(quantity)*idli(4)
-    # elif food.lower=="basmati":
-    #     return "Calories " + int(quantity)*basmati(0)  + "Carbs " + int(quantity)*basmati(1) + "Fats " + int(quantity)*basmati(2) + "Protein " + int(quantity)*basmati(3) + "Sodium " + int(quantity)*basmati(4)
+    elif food.lower()=="dosa":
+        return "Calories " + str(int(quantity)*dosa[0])  + " Carbs " + str(int(quantity)*dosa[1]) + " Fats " + str(int(quantity)*dosa[2]) + " Protein " + str(int(quantity)*dosa[3]) + " Sodium " + str(int(quantity)*dosa[4])
+    elif food.lower()=="uttapam":
+        return "Calories " + str(int(quantity)*uttapam[0])  + " Carbs " + str(int(quantity)*uttapam[1]) + " Fats " + str(int(quantity)*uttapam[2]) + " Protein " + str(int(quantity)*uttapam[3]) + " Sodium " + str(int(quantity)*uttapam[4])
+    elif food.lower()=="idli":
+        return "Calories " + str(int(quantity)*idli[0])  + " Carbs " + str(int(quantity)*idli[1]) + " Fats " + str(int(quantity)*idli[2]) + " Protein " + str(int(quantity)*idli[3]) + " Sodium " + str(int(quantity)*idli[4])
+    elif food.lower()=="basmati rice":
+        return "Calories " + str(int(quantity)*basmati[0])  + " Carbs " + str(int(quantity)*basmati[1]) + " Fats " + str(int(quantity)*basmati[2]) + " Protein " + str(int(quantity)*basmati[3]) + " Sodium " + str(int(quantity)*basmati[4])
     else:
-        return "We have failed again!!!!"
-        #return "Calories " + str(int(quantity)*biryani[0])  + "Carbs " + str(int(quantity)*biryani[1]) + "Fats " + str(int(quantity)*biryani[2]) + "Protein " + str(int(quantity)*biryani[3]) + "Sodium " + str(int(quantity)*biryani[4])
-        #print("Calories " + int(quantity)*biryani(0)  + "Carbs " + int(quantity)*biryani(1) + "Fats " + int(quantity)*biryani(2) + "Protein " + int(quantity)*biryani(3) + "Sodium " + int(quantity)*biryani(4))
+        return "SYNTAX ERROR: I'm sorry this version of IndCalc does not support this, try again"
