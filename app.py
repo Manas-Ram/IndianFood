d477ccad81a07311c399629ea3e216d53b3cc0af
Manas-Ram <manas.ramesh24@gmail.com>
2020-07-31 14:29:02 +0000

    heroku

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import get_nut_value
 
 
 # -- Initialization section --
@@ -13,4 +14,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+
+@app.route('/result', methods= ['GET', 'POST'])
+def result():
+    print(request.form)
+    ind_food = (request.form["food"])
+    quant_v=(request.form["quantity"])
+    nut_v=get_nut_value(ind_food, quant_v)
+    return render_template("result.html", ind_food=ind_food, quant_v=quant_v, nut_v=nut_v)
+
+
